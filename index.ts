56e66e9fa730c27eb1c002a262d21937139a06d1
Beau Davenport <rdavenport@linkedin.com>
2022-05-08 18:32:10 -0700

    mvp

--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
-export default () => {}; 
+const preduce = <T, U>(array: Array<T>, initialValue: U, reducer: (initial: U, current: T, idx?: number) => U) => array.reduce(reducer, initialValue);
+
+export default preduce;
